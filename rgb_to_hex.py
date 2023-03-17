aaaf644a9467bdda2e263b42086dbb901e5249b2
Kendall <25021027+kefortney@users.noreply.github.com>
2023-03-17 19:17:12 +0000

    rgb python file added

---
+++ rgb_to_hex.py
@@ -0,0 +1,9 @@
+def rgb_to_hex(r, g, b):
+    r = max(0, min(255, r))
+    g = max(0, min(255, g))
+    b = max(0, min(255, b))
+    return '{:02X}{:02X}{:02X}'.format(r, g, b)
+
+
+hex_color = rgb_to_hex(255, 127, 0) # returns "FF7F00"
+print(hex_color)
