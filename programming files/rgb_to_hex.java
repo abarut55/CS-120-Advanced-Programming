14d2a6362bdc52680abc066602eee4b7a432f1b1
abarut55 <112417990+abarut55@users.noreply.github.com>
2023-03-21 10:31:32 -0400

    Update rgb_to_hex.java
     added extra ;

--- programming files/rgb_to_hex.java
+++ programming files/rgb_to_hex.java
@@ -11,7 +11,8 @@ public class rgb_to_hex {
         r = Math.min(255, Math.max(0, r));
         g = Math.min(255, Math.max(0, g));
         b = Math.min(255, Math.max(0, b));
-        return String.format("%02X%02X%02X", r, g, b);
+        // added extra ;
+        return String.format("%02X%02X%02X", r, g, b);;
     }
 }
 
